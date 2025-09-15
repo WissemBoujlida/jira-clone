9ae470fcc1892ff374fd37dcf66167b1b242d621
WissemBoujlida <wissem.boujlida@esprit.tn>
2025-09-15 22:49:45 +0100

    set up Hono API

---
+++ src/app/api/[[...route]]/route.ts
@@ -0,0 +1,18 @@
+import { Hono } from "hono";
+import { handle } from "hono/vercel";
+
+const app = new Hono().basePath("/api");
+
+app.get("/hello", (c) => {
+  return c.json({
+    message: "Hello Hono!",
+  });
+});
+
+app.get("/users/:userId", (c) => {
+  const { userId } = c.req.param();
+
+  return c.json({ user: userId });
+});
+
+export const GET = handle(app);
